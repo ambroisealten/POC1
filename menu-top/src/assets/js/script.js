9bc3405cd01a248d3392d371b9a3993b01526fa6
ThomasDecampAlten <48323182+ThomasDecampAlten@users.noreply.github.com>
2019-03-15 17:28:42 +0100

    Barre menu modules

--- menu-top/src/assets/js/script.js
+++ menu-top/src/assets/js/script.js
@@ -2,4 +2,19 @@ window.onload = charged;
 
 function charged(){
     
+    let moduleHome = document.getElementById("moduleHome");
+    moduleHome.addEventListener("mouseover", function(e){
+        let moduleLink = moduleHome.firstChild;
+        let modules = moduleLink.nextSibling;
+        moduleLink.style.display = "none";
+        modules.style.display = "grid";
+    });
+
+    moduleHome.addEventListener('mouseout',function(e){
+        let moduleLink = moduleHome.firstChild;
+        let modules = moduleLink.nextSibling;
+        moduleLink.style.display = "grid";
+        modules.style.display = "none";
+    })
+    
 }
