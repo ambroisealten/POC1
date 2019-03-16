c95975999fefd35a8951718b4e0d42b9e5e4d904
lucasroyackkers <48323570+lucasroyackkers@users.noreply.github.com>
2019-03-16 23:16:33 +0100

    Merge Menu-Top & Auto-complétion
    Ajout des champs d'auto-compétion (+ ajout de style et animations)

--- menu-top/src/app/app.module.ts
+++ menu-top/src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router'
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material';
+import { DemoMaterialModule } from '../material-module';
+import { Routes, RouterModule } from '@angular/router';
+import { SearchAutoComponent } from './search-auto/search-auto.component';
+import { SearchAutoGroupComponent } from './search-auto-group/search-auto-group.component'
+import { SearchService } from './services/search.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchAutoComponent,
+    SearchAutoGroupComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    DemoMaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [ 
+    SearchService
   ],
   bootstrap: [AppComponent]
 })
--- menu-top/src/assets/js/script.js
+++ menu-top/src/assets/js/script.js
@@ -1,20 +1,36 @@
 window.onload = charged;
 
 function charged(){
-    
-    let moduleHome = document.getElementById("moduleHome");
-    moduleHome.addEventListener("mouseover", function(e){
-        let moduleLink = moduleHome.firstChild;
-        let modules = moduleLink.nextSibling;
-        moduleLink.style.display = "none";
-        modules.style.display = "grid";
-    });
+    setModuleHome();
+    setHeadMenus();
+}
 
-    moduleHome.addEventListener('mouseout',function(e){
-        let moduleLink = moduleHome.firstChild;
-        let modules = moduleLink.nextSibling;
-        moduleLink.style.display = "grid";
-        modules.style.display = "none";
-    })
-    
+function setModuleHome(){
+    let moduleHome = document.getElementById("moduleHome");
+    if(moduleHome != undefined){
+        moduleHome.addEventListener("mouseover", function(e){
+            moduleHome.firstChild.style.display = "none";
+            moduleHome.firstChild.nextSibling.style.display = "grid";
+        });
+        moduleHome.addEventListener('mouseout',function(e){
+            moduleHome.firstChild.style.display = "grid";
+            moduleHome.firstChild.nextSibling.style.display = "none";
+        });
+    }
+}
+
+function setHeadMenus(){
+    let headMenus = document.getElementsByClassName('headMenu');
+    for(let headMenu of headMenus){
+        headMenu.addEventListener("mouseover",function(e){
+            headMenu.setAttribute('class',headMenu.getAttribute('class')+' show');
+            headMenu.firstChild.setAttribute('aria-expanded',true);
+            headMenu.firstChild.nextSibling.setAttribute('class',headMenu.firstChild.nextSibling.getAttribute('class')+' show');
+        });
+        headMenu.addEventListener("mouseout",function(e){
+            headMenu.setAttribute('class',headMenu.getAttribute('class').substr(0,headMenu.getAttribute('class').length-5)); 
+            headMenu.firstChild.setAttribute('aria-expanded',false);
+            headMenu.firstChild.nextSibling.setAttribute('class',headMenu.firstChild.nextSibling.getAttribute('class').substr(0,headMenu.firstChild.nextSibling.getAttribute('class').length-5));    
+        })
+    }
 }
