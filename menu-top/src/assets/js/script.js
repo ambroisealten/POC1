2ab623e6d6b5a0ada8d93144dcdc746b6de479c6
lucasroyackkers <48323570+lucasroyackkers@users.noreply.github.com>
2019-03-20 17:46:16 +0100

    Modularité design header
    Premier jet de taille de police adaptative à la taille de l'écran de l'utilisateur

--- menu-top/src/assets/js/script.js
+++ menu-top/src/assets/js/script.js
@@ -28,20 +28,32 @@ function setModuleHome(){
     }
 }
 
+function handler(e){
+    let parentNode = e.target.parentNode;
+    if(parentNode.getAttribute('class').includes('headMenu')){
+        if(parentNode.getAttribute('class').includes('show')){
+            parentNode.setAttribute('class',parentNode.getAttribute('class')+' show');
+            parentNode.firstChild.setAttribute('aria-expanded',true);
+            parentNode.firstChild.nextSibling.setAttribute('class',parentNode.firstChild.nextSibling.getAttribute('class')+' show');
+        } 
+    }
+}
+
 function setHeadMenus(){
     setTimeout(() =>{
         let headMenus = document.getElementsByClassName('headMenu');
         for(let headMenu of headMenus){
+            headMenu.addEventListener('click',handler,true);
             headMenu.addEventListener("mouseover",function(e){
                 headMenu.setAttribute('class',headMenu.getAttribute('class')+' show');
                 headMenu.firstChild.setAttribute('aria-expanded',true);
                 headMenu.firstChild.nextSibling.setAttribute('class',headMenu.firstChild.nextSibling.getAttribute('class')+' show');
-            });
+            },true);
             headMenu.addEventListener("mouseout",function(e){
-                headMenu.setAttribute('class',headMenu.getAttribute('class').substr(0,headMenu.getAttribute('class').length-5)); 
-                headMenu.firstChild.setAttribute('aria-expanded',false);
-                headMenu.firstChild.nextSibling.setAttribute('class',headMenu.firstChild.nextSibling.getAttribute('class').substr(0,headMenu.firstChild.nextSibling.getAttribute('class').length-5));    
-            })
+                if(headMenu.getAttribute('class').includes(" show")) headMenu.setAttribute('class',headMenu.getAttribute('class').substr(0,headMenu.getAttribute('class').length-5)); 
+                if(headMenu.firstChild.getAttribute('aria-expanded')) headMenu.firstChild.getAttribute('aria-expanded',false);
+                if(headMenu.firstChild.nextSibling.getAttribute('class').includes(" show")) headMenu.firstChild.nextSibling.setAttribute('class',headMenu.firstChild.nextSibling.getAttribute('class').substr(0,headMenu.firstChild.nextSibling.getAttribute('class').length-5));
+            },true);
         };
     },50);
 }
